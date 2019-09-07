2bd51c4355ead16dc5c368154f0dc83c439d6b4c
mfcarneiro <matheus.felipe123@gmail.com>
2019-09-07 15:13:14 -0300

    Login screen layout

---
+++ app/views/login/Login.vue
@@ -0,0 +1,15 @@
+<template>
+	<Page>
+		<ActionBar actionBarHidden="true" />
+		<StackLayout orientation="vertical">
+			<TextView :autocorrect="false" hint="Login" />
+			<TextField hint="Password" text="Password" secure="true" />
+		</StackLayout>
+	</Page>
+</template>
+
+<script src="./login.js">
+</script>
+
+<style src="./login.scss"  lang="scss" scoped>
+</style>
---
+++ app/views/login/login.js
@@ -0,0 +1,14 @@
+import loginModel from '../../models/loginModel';
+
+export default {
+	data() {
+		return {
+			key: value,
+		};
+	},
+	computed: {
+		name() {
+			return this.data;
+		},
+	},
+};
